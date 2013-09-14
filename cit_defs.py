e93dbc63ea23b28074b254805f056c25f9bbed91
David Jinkins <david.jinkins@gmail.com>
2013-09-14 12:29:54 -0400

    added the cython files, finshed tinkering with speed, ready to work on putting it all together.

--- cit_defs.py
+++ cit_defs.py
@@ -13,13 +13,10 @@ def tree():
     return collections.defaultdict(tree)
 
 def no_cit_inner(row, alp, bet, dep_year):
-    print row
     k_lev = dep_year.at[row['dep'],row['date']-1]
-    num0 = alp[0] + bet * k_lev
-    item0 = 1 - num0 / (1 + num0)
-    num1 = alp[1] + bet * k_lev
-    item1 = 1 - num1 / (1 + num1)
-    return item0, item1
+    num = alp + bet * k_lev
+    item = 1 - num / (1 + num)
+    return item
 
 def cit_inner(row, alp, bet, dep_year):
     num = alp + bet * dep_year.at[row['dep'],row['date']-1]
@@ -32,12 +29,19 @@ def cit_lik_no_cit(alp, bet, gam, lp, dep_aut, dep_year):
     lin1 = dep_aut.iloc[0]
     palp = alp[lin1['qual']][lin1['isField']]
     pgam = gam[lin1['qual']][lin1['isField']]
-    liks =  dep_aut.apply(lambda row: no_cit_inner(row, palp,
-                                                   bet, dep_year), axis=1)
-
-    arg0 = (1 - pgam[0] + pgam[0] * liks[0].prod())
-    arg1 = (1 - pgam[1] + pgam[1] * liks[1].prod())
-    return math.log(lp * arg0 + (1 - lp) * arg1)
+    liks = []
+    liks.append(dep_aut.apply(lambda row: no_cit_inner(row, palp[0],
+                                                   bet, dep_year), axis=1))
+    # check for latent type possibility
+    if lp != 0:
+        liks.append(dep_aut.apply(lambda row: no_cit_inner(row, palp[1],
+                                                   bet, dep_year), axis=1))
+        arg0 = (1 - pgam[0] + pgam[0] * liks[0].prod())
+        arg1 = (1 - pgam[1] + pgam[1] * liks[1].prod())
+        return math.log(lp * arg0 + (1 - lp) * arg1)
+    else:
+        arg0 = (1 - pgam[0] + pgam[0] * liks[0].prod())
+        return math.log(arg0)
 
 def cit_lik_cit(alp, bet, gam, lp, dep_aut, dep_year):
     # calculates a single cit authors lik
@@ -45,8 +49,11 @@ def cit_lik_cit(alp, bet, gam, lp, dep_aut, dep_year):
     lin1 = dep_aut.iloc[0]
     palp = alp[lin1['qual']][lin1['isField']]
     pgam = gam[lin1['qual']][lin1['isField']]
-    liks =  dep_aut.apply(lambda row: no_cit_inner(row, palp,
-                                                   bet, dep_year), axis=1)
+    liks = []
+    liks.append(dep_aut.apply(lambda row: no_cit_inner(row, palp[0],
+                                                   bet, dep_year), axis=1))
+    liks.append(dep_aut.apply(lambda row: no_cit_inner(row, palp[1],
+                                                   bet, dep_year), axis=1))
     arg0 = (pgam[0] * liks[0].prod())
     arg1 = (pgam[1] * liks[1].prod())
     return math.log(lp * arg0 + (1 - lp) * arg1)
@@ -55,11 +62,16 @@ def fc_lik(alp, bet, gam, lp, dep_aut, dep_year):
     # calculates first cite likelihoods
 
     lin1 = dep_aut.iloc[0]
-    palp = alp[lin1['qual']][lin1['isField']][0]
-    pgam = gam[lin1['qual']][lin1['isField']][0]
-    liks = dep_aut.apply(lambda row: cit_inner(row, palp,
-                                                   bet, dep_year), axis=1)
-    return (math.log(pgam * liks))
+    palp = alp[lin1['qual']][lin1['isField']]
+    pgam = gam[lin1['qual']][lin1['isField']]
+    liks = []
+    liks.append(dep_aut.apply(lambda row: cit_inner(row, palp[0],
+                                                   bet, dep_year), axis=1))
+    liks.append(dep_aut.apply(lambda row: cit_inner(row, palp[1],
+                                                   bet, dep_year), axis=1))
+    arg0 = (pgam[0] * liks[0].prod())
+    arg1 = (pgam[1] * liks[1].prod())
+    return math.log(lp * arg0 + (1 - lp) * arg1)
 
 dep_year = pd.read_pickle('dep_years.pickle')
 aut_pan = pd.read_pickle('initial_panel.pickle')
@@ -80,7 +92,6 @@ for qual in range(3):
             alp[qual][field][lat] = 0.1
             gam[qual][field][lat] = 0.1
 
-
 tot = 0
 tik = clock()
 cit_liks = citers.groupby('au')\
@@ -88,37 +99,11 @@ cit_liks = citers.groupby('au')\
 fc_liks = first_cits.groupby('au')\
             .apply(lambda x: fc_lik(alp, 100, gam, 0.1,  x, dep_year))
 nocit_liks = nocits.groupby('au')\
-            .apply(lambda x: cit_lik_no_cit(alp, 100, gam, 0.1, x, dep_year))
+            .apply(lambda x: cit_lik_no_cit(alp, 100, gam, 0, x, dep_year))
 
 tot += cit_liks.sum() + fc_liks.sum() + nocit_liks.sum()
 print tot
 tok = clock() - tik
 print tok
-
-
-# tot = 0
-# tik = clock()
-# for qual in range(3):
-#     for field in range(2):
-#         cit_liks = citers[(citers['isField'] == field)
-#                              & (citers['qual'] == qual)]\
-#                             .groupby('au')\
-#                             .apply(lambda x: cit_lik_cit(.1, 100, .1,
-#                                                          x, dep_year))
-#         fc_liks = first_cits[(first_cits['isField'] == field)
-#                              & (first_cits['qual'] == qual)]\
-#                             .groupby('au')\
-#                             .apply(lambda x: fc_lik(.1, 100, .1,
-#                                                          x, dep_year)) 
-#         nocit_liks = nocits[(nocits['isField'] == field)
-#                              & (nocits['qual'] == qual)]\
-#                             .groupby('au')\
-#                             .apply(lambda x: cit_lik_no_cit(.1, 100, .1,
-#                                                          x, dep_year))
-#         tot += cit_liks.sum() + fc_liks.sum() + nocit_liks.sum()
-# print tot
-# tok = clock() - tik
-# print tok
-
 
 
