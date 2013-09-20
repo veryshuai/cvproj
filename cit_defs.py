cef51602cb85f55f6d1cbfa437f50810d3c9e568
David Jinkins <david.jinkins@gmail.com>
2013-09-20 08:53:26 -0400

    updated calculation of probabilities to reflect new latent type method

--- cit_defs.py
+++ cit_defs.py
@@ -14,9 +14,9 @@ import numpy as np
 def tree():
     return collections.defaultdict(tree)
 
-def no_cit_inner(row, alp, bet, dep_year):
+def no_cit_inner(row, alp, bet, dep_year, lat):
     k_lev = dep_year.at[row['dep'],row['date']-1]
-    num = alp + bet * k_lev
+    num = alp + bet * k_lev + lat
     item = 1 - math.exp(num) / (1 + math.exp(num))
     return item
 
@@ -29,45 +29,38 @@ def cit_lik_no_cit(alp, bet, gam, dep_aut, dep_year, lat):
     # calculates a single no cit authors lik
 
     lin1 = dep_aut.iloc[0]
-    if lin1['isField'] == 0 and lat == 1:
-        return 0 # tricky way of returning zeros
-    else:
-        palp = alp[0][lin1['isField']][lat]
-        pgam = gam[0][lin1['isField']][lat]
-        pbet = bet[0][lin1['isField']][lat]
-        liks = dep_aut.apply(lambda row: no_cit_inner(row, palp,
-                                                       pbet, dep_year), axis=1)
-        arg = (1 - pgam + pgam * liks.prod())
-        return arg
+    palp = alp[0][lin1['isField']][0]
+    pgam = gam[0][lin1['isField']][0]
+    pbet = bet[0][lin1['isField']][0]
+    liks = dep_aut.apply(lambda row: no_cit_inner(row, palp,
+                                                   pbet, dep_year,
+                                                   lat), axis=1)
+    arg = (1 - pgam + pgam * liks.prod())
+    return arg
 
 def cit_lik_cit(alp, bet, gam, dep_aut, dep_year, lat):
     # calculates a single cit authors lik
 
     lin1 = dep_aut.iloc[0]
-    if lin1['isField'] == 0 and lat == 1:
-        return 0 # tricky way of returning zeros
-    else:
-        palp = alp[0][lin1['isField']][lat]
-        pgam = gam[0][lin1['isField']][lat]
-        pbet = bet[0][lin1['isField']][lat]
-        liks = dep_aut.apply(lambda row: no_cit_inner(row, palp,
-                                                   pbet, dep_year), axis=1)
-        arg = (pgam * liks.prod())
-        return arg
+    palp = alp[0][lin1['isField']][0]
+    pgam = gam[0][lin1['isField']][0]
+    pbet = bet[0][lin1['isField']][0]
+    liks = dep_aut.apply(lambda row: no_cit_inner(row, palp,
+                                               pbet, dep_year,
+                                               lat), axis=1)
+    arg = (pgam * liks.prod())
+    return arg
 
 def fc_lik(alp, bet, gam, dep_aut, dep_year, lat):
     # calculates first cite likelihoods
 
     lin1 = dep_aut.iloc[-1]
-    if lin1['isField'] == 0 and lat == 1:
-        return 0 # tricky way of returning zeros
-    else:
-        palp = alp[0][lin1['isField']][lat]
-        pgam = gam[0][lin1['isField']][lat]
-        pbet = bet[0][lin1['isField']][lat]
-        num = palp + pbet * dep_year.at[lin1['dep'],lin1['date']-1]
-        item = math.exp(num) / (1 + math.exp(num))
-        return item
+    palp = alp[0][lin1['isField']][0]
+    pgam = gam[0][lin1['isField']][0]
+    pbet = bet[0][lin1['isField']][0]
+    num = palp + lat + pbet * dep_year.at[lin1['dep'],lin1['date']-1]
+    item = math.exp(num) / (1 + math.exp(num))
+    return item
 
 def trans_prob(row, t):
     # retrieves correct value from transition prob matrix
@@ -90,47 +83,3 @@ def mov_lik(trans, group, lat):
         else:
             lik = group.apply(lambda row: trans_prob(row, t), axis=1)
             return lik.prod()
-
-
-# aut_pan = pd.read_pickle('initial_panel.pickle')
-# dep_year = pd.read_pickle('dep_years.pickle')
-# first_cits = pd.read_pickle('first_cits.pickle')
-# citers = pd.read_pickle('citers.pickle')
-# nocits = pd.read_pickle('nocits.pickle')
-# mov_dat = pd.read_pickle('mov_dat.pickle')
-# f = file('trans.pickle','rb')
-# trans = pickle.load(f)
-# 
-# # MOVE LIKS
-# tik = clock()
-# mlik = []
-# for lat in range(2):
-#     mlik.append(mov_dat.groupby('au').apply(lambda x: mov_lik(trans, x, lat)))
-# tok = clock() - tik
-# print tok
-# 
-# alp = tree()
-# gam = tree()
-# for qual in range(3):
-#     for field in range(2):
-#         for lat in range(2):
-#             alp[qual][field][lat] = 0.1
-#             gam[qual][field][lat] = 0.1
-# 
-# tot = 0
-# tik = clock()
-# cit_liks, fc_liks, nocit_liks = [], [], []
-# for lat in range(2):
-#     cit_liks.append(citers.groupby('au')\
-#                 .apply(lambda x: cit_lik_cit(alp, 100, gam, 0.1, x, dep_year, lat)))
-#     fc_liks.append(first_cits.groupby('au')\
-#                 .apply(lambda x: fc_lik(alp, 100, gam, 0.1,  x, dep_year, lat)))
-#     nocit_liks.append(nocits.groupby('au')\
-#                 .apply(lambda x: cit_lik_no_cit(alp, 100, gam, 0, x, dep_year, lat)))
-# 
-# #tot += cit_liks.sum() + fc_liks.sum() + nocit_liks.sum()
-# print tot
-# tok = clock() - tik
-# print tok
-
-
