3d66cb1fcd92ec0c01eeae6a90f8cbfde8b58e52
David Jinkins <david.jinkins@gmail.com>
2015-07-15 11:58:59 +0200

    changed startpoints

--- cost_version/run_discrete.py
+++ cost_version/run_discrete.py
@@ -12,20 +12,20 @@ def tree():
 if __name__ == '__main__':
     """reads in parameters and calls discrete.py, main estimation"""
 
-    alp = [-1.6912994607298211]
-    bet = [10.829271047737196]
-    gam = [0.034228248703075864, 0.098392799712779644]
+    alp = [-0.56945452531275809]
+    bet = [15.602400261243089]
+    gam = [0.038372248406727365,0.079477727422792799]
     
     # INITIAL MOV PARAMETERS 
-    mov_params = pd.Series({'qual': 0.118713564513042, 'field': 1.1773030907893029, 'lat': -1.16855264979563331})
+    mov_params = pd.Series({'qual': 0.27344725062483821, 'field': 1.1834150397048537, 'lat': -1.0034251581340474})
     
     # OTHER PARAMETERS 
-    lp = [-0.8,-0.82761089372683,0.325140952129204] #latent type probability,
+    lp = [-1.5470558565886836,-1.0702211065628657,0.36723437029046224] #latent type probability,
     # two mean parameters and a standard deviation
-    lo = 8.3 #offer arrival rate,
+    lo = 8.3066733345055646 #offer arrival rate,
     # base and qual dependence
     p = 0 # 1.282008407975466 #signing bonus distribution parameter
-    ip = 4.044689899777988 #instrument parameter (affects 1991 wages)
+    ip = 4.0413296516360013 #instrument parameter (affects 1991 wages)
     
     # PUT PARAMS INTO BOXES FOR EASY MOVING
     big_mov_params = [mov_params, lo, p]
